13f682198ec8f9c76782d929cad0ea4a0bfad643
ZerefeL <zerefel@gmail.com>
2015-05-22 18:40:34 +0300

    Fixed some nasty bugs

---
+++ app/js/controllers/uploadController.js
@@ -0,0 +1,14 @@
+SocialNetwork.controller('UploadController', function ($scope, fileReader) {
+    $scope.getFile = function () {
+        $scope.progress = 0;
+        fileReader.readAsDataUrl($scope.file, $scope)
+            .then(function(result) {
+                $scope.userData.profileImageData = result;
+            });
+    };
+
+    $scope.$on("fileProgress", function(e, progress) {
+        $scope.progress = progress.loaded / progress.total;
+    });
+
+});
